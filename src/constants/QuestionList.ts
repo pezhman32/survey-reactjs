6742acba69c26a9b6f2be89276448485244eb933
pezhman.jahanmard <pezhman.jahanmard@innogames.com>
2019-03-02 22:35:30 +0100

    Update questions

--- src/constants/QuestionList.ts
+++ src/constants/QuestionList.ts
@@ -1,7 +1,8 @@
 import { QuestionMode, QuestionType } from '../components/Question/QuestionType';
 
 export const QUESTION_LIST: QuestionType[] = [
-  { title: 'How old are you?', mode: QuestionMode.TEXT },
+  { title: 'What is your name?', mode: QuestionMode.TEXT },
   { title: 'What is your gender?', mode: QuestionMode.SINGLE_DROP_DOWN, options: ['Male', 'Female', 'Other'] },
+  { title: 'Is this survey well implemented?', mode: QuestionMode.SINGLE_RADIO, options: ['Yes', 'Sure', 'This Is Awesome'] },
   { title: 'Which one do you prefer?', mode: QuestionMode.SINGLE_RADIO, options: ['Mac', 'Windows', 'Linux'] },
 ];
